3665bcc6d16ca02ed86a39e2c22162e36669e0fd
JRJ <jjsocc171@msn.com>
2021-03-16 21:18:21 -0500

    created routes for homepage

--- models/Comment.js
+++ models/Comment.js
@@ -2,12 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
-
 Comment.init(
   {
-    //columns will go here
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
@@ -20,15 +19,13 @@ Comment.init(
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'user',
         key: 'id'
-      }  
+      }
     },
     post_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'post',
         key: 'id'
