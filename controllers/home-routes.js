0efbeb100e638d6b8cb06c5c1b474d834e9ad2e9
JRJ <jjsocc171@msn.com>
2021-03-17 19:13:43 -0500

    added login/logout routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  console.log('=========================');
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -40,6 +40,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 
