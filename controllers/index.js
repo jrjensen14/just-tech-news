291319c6fe6d9c8bbcf4d93a768065de26c123a7
JRJ <jjsocc171@msn.com>
2021-03-18 19:24:25 -0500

    added comment/upvote routes

--- controllers/index.js
+++ controllers/index.js
@@ -6,9 +6,4 @@ const apiRoutes = require('./api');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-
-// router.use((req, res) => {
-//   res.status(404).end();
-// });
-
 module.exports = router;
